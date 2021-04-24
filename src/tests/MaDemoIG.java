b2135cf9e6039d37ab569861f8ee6e8543ea9053
Kadri Ryan <ryan_kadri@ens.univ-artois.fr>
2021-04-24 13:49:11 +0200

    init projet

---
+++ src/tests/MaDemoIG.java
@@ -0,0 +1,9 @@
+package tests;
+
+import grafix.interfaceGraphique.IG;
+
+public class MaDemoIG {
+    public static void main(String[] args) {
+        Object[] parametres = IG.saisirParametres();
+    }
+}
